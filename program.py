f9096e37107fb0b96ca5ead884a4f534b0b0898b
rapost <mindrobots@gmail.com>
2016-08-31 14:01:12 -0400

    add main() function

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
-print('-----------------------------')
-print('          HELLO APP')
-print('-----------------------------')
-print()
+def main():
+    print('-----------------------------')
+    print('          HELLO APP')
+    print('-----------------------------')
+    print()
 
-print('Nice to meet you ' + input("What is your name? "))
+    print('Nice to meet you, ' + input("What is your name? "))
+
+
+if __name__ == '__main__':
+    main()
